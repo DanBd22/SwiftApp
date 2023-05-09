caa1b8b878252753acdbe796a4e7490148d6adb4
DanBd22 <dan.badarau04@gmail.com>
2023-05-09 13:43:33 +0300

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Badarau Dan on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
